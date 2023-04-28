a32b286853979ea2549a63e2b35da7120cf01d5f
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-28 16:10:35 -0500

    added mRNA applicaton to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,8 +17,6 @@ const Projects = () => {
             <h2>Marvel</h2>
             <p>Search, collect, and comment on your favorite Marvel characters and comics.</p>
             <p><b>Languages </b>Python, Flask, Jinja, PostgreSQL</p>
-            <p style={{hover: "pointer"}}>Click to visit</p>
-
           </div>
           <div className="projects-orbiter"></div>
         </div></a>
@@ -31,21 +29,31 @@ const Projects = () => {
             <h2>ParkExplorerPro</h2>
             <p>Explore and search national parks to find campgrounds, weather, articles, visitor centers, things to do, events, and more.</p>
             <p><b>Languages </b>React, Node, Express</p>
-            <p style={{ hover: "pointer" }}>Click to visit</p>
-
           </div>
           <div className="projects-orbiter"></div>
         </div></a>
 
         <a href="http://computersciencevisualizer.herokuapp.com/" target="_blank"><div class="projects-square">
           <div className='projects-background'></div>
-          <h4>ComputerScienceVisualizer.com</h4>
+          <h4>C.S. Visualizer</h4>
           <div className='projects-content'>
             <div className="csv-icon"></div>
-            <h2>ComputerScienceVisualizer.com</h2>
+            <h2>ComputerScience</h2>
+            <h2>Visualizer.com</h2>
             <p>Learn to use data structures like stacks, queues, and linked-lists easily with our visualization tool.</p>
             <p><b>Languages </b>React, JavaScript</p>
-            <p style={{ hover: "pointer" }}>Click to visit</p>
+          </div>
+          <div className="projects-orbiter"></div>
+        </div></a>
+
+        <a href="https://mrna-translation-visualizer.herokuapp.com/" target="_blank"><div class="projects-square">
+          <div className='projects-background'></div>
+          <h4>mRNA Translator</h4>
+          <div className='projects-content'>
+            <div className="mrna-icon"></div>
+            <h2>mRNA Translation Visualizer</h2>
+            <p>Visualize the process that messenger RNA undergoes to build a protein. Science geeks welcome.</p>
+            <p><b>Languages </b>React, JavaScript</p>
           </div>
           <div className="projects-orbiter"></div>
         </div></a>
